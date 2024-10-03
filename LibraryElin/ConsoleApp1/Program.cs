16171536f7806e7f70cba9c27cc8dc40ca00d126
Константин Елин <mrvide@mail.ru>
2024-10-03 14:46:23 +0500

    Completed work

---
+++ LibraryElin/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using LibraryElin;
+using static LibraryElin.Class1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 myFunctions = new Class1();
+
+            int a = 10;
+            int b = 5;
+
+            Console.WriteLine("Add: " + myFunctions.Add(a, b));
+            Console.WriteLine("Subtract: " + myFunctions.Subtract(a, b));
+            Console.WriteLine("Multiply: " + myFunctions.Multiply(a, b));
+            Console.WriteLine("Divide: " + myFunctions.Divide(a, b));
+            Console.WriteLine("Modulus: " + myFunctions.Modulus(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
